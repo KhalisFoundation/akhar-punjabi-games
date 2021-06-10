ba35c17af9e97ff431402a06d70a3eec372ac2f6
Gian <giansingh4710@gmail.com>
2021-06-09 20:05:49 -0400

    last file wasn't expo, so had to make this new one

--- wordGameReactNative/App.js
+++ wordGameReactNative/App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
 
 export default function App() {
+  const [fatheList, setList] = React.useState([
+    "ਵਾਹਿਗੁਰੂਜੀਕਾਖਾਲਸਾਵਾਹਿਗੁਰੂਜੀਕੀਫਤੇ||",
+  ]);
+
+  const [count, setCount] = React.useState(0);
+  function printLst() {
+    let lst = [];
+    for (let i = 0; i < count; i++) {
+      lst.push(<Text>ਵਾਹਿਗੁਰੂਜੀਕਾਖਾਲਸਾਵਾਹਿਗੁਰੂਜੀਕੀਫਤੇ||</Text>);
+    }
+    return lst;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {printLst()}
+      <Button
+        title="CLICK"
+        onPress={() => {
+          setCount((prev) => prev + 1);
+        }}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
