fabb4e857387733dbfb7f3152cce23f83709b319
Gian <giansingh4710@gmail.com>
2021-06-15 12:02:26 -0400

    data scraping things

--- DataScraping/scrapeData.py
+++ DataScraping/scrapeData.py
@@ -5,16 +5,14 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.headless = True
 
+
+#take out sentences. only words
 class ByMatra():
-    def getAllLinks(self):
-        
-        linksLst=[f"http://www.discoversikhism.com/punjabi/{i}.html".lower() for i in lst]
-        return linksLst
 
     def scrapeLink(self):
         br =  webdriver.Chrome('C:\\Users\\gians\\Desktop\\stuff\\chromedriver.exe',options=options)
-        allLinks=self.getAllLinks()
         lst=["gurmukhi_word_list","Kanna_words","Sihari_words","Bihari_words","Aunkar_words","Dulainkar_words","Lava_words","Dulava_words","Hora_words","Kanaura_words"]
+        allWords=[]
         for matra in lst:
             theLink=f"http://www.discoversikhism.com/punjabi/{matra}.html".lower()
             br.get(theLink)
@@ -23,8 +21,13 @@ class ByMatra():
             thePage=soup.find("div",id="content2")
             allTables=thePage.findAll("table")
             for table in range(len(allTables)):
-                print(f"Table {table}:")
                 words=self.getDataFromTables(allTables[table])
+                allWords+=words
+        f=open("./DataScraping/data.csv","a",newline="",encoding= "utf-8")
+        writer=csv.writer(f)
+        allWords=[i for i in allWords if len(i[0].split())==1]
+        writer.writerows(allWords)
+        f.close()
 
     def getDataFromTables(self,table):
         rows=table.findAll("tr")
@@ -41,15 +44,32 @@ class ByMatra():
         return allWords
     
 
+class ThousandMostCommonWords():
+    def scrapeLink(self):
+        link="https://1000mostcommonwords.com/1000-most-common-punjabi-words/"
+        res=requests.get(link)
+        soup=BeautifulSoup(res.text,"lxml")
+        table=soup.find("table")
+        trs=table.findAll("tr")[1:]
+        allWords=[]
+        for tr in trs:
+            tds=tr.findAll("td")
+            punjabiWord=tds[1].text
+            englishWord=tds[2].text
+            word=(punjabiWord,englishWord)
+            allWords.append(word)
+        f=open("./DataScraping/data.csv","a",newline="",encoding= "utf-8")
+        writer=csv.writer(f)
+        allWords=[i for i in allWords if len(i[0].split())==1]
+        writer.writerows(allWords)
+        f.close()
 
-link1="http://www.discoversikhism.com/punjabi/gurmukhi_word_list.html"
-link2="https://1000mostcommonwords.com/1000-most-common-punjabi-words/"
 
-a=ByMatra()
-a.scrapeLink()
+lst=[ByMatra(),ThousandMostCommonWords()]
+[i.scrapeLink() for i in lst]
 
 # f=open("./DataScraping/data.csv","a",newline="")
-# tupa=[("bpb",19),("hello",123),(21,32,43)]
+# tup=[("bpb",19),("hello",123),(21,32,43)]
 # writer=csv.writer(f)
-# writer.writerows(tupa)
+# writer.writerows(tup)
 # f.close()
