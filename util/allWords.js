af5ba63c1bd20eb2f63d3203f9e4e7ef2c4e130b
Giansingh4710 <73843250+Giansingh4710@users.noreply.github.com>
2021-06-24 16:54:47 -0400

    Now with reducer (#33)
    * rerending problems
    
    * connects
    
    * with ascii
    
    * latest
    
    * letters connect together
    
    * sehari fix
    
    * linted
    
    * trying to use reducer
    
    * replaced useState with reducer. Also added button to generate new word
    
    * the state
    
    * for lint
    
    * more lint changes
    
    * deleted things that are not used
    
    * linted
    
    * linted
    
    * linted

---
+++ util/allWords.js
@@ -0,0 +1,44 @@
+export const words = [
+  {
+    text: "ਨਾਮ",
+    meaning: "Name, God Consciousness, Divine Presence",
+    ascii: [2600, 2622, 2606],
+  },
+  {
+    text: "ਰਹਾੳੁ",
+    meaning: "Pause, (marks chorus, or main theme line)",
+    ascii: [2608, 2617, 2622, 2675, 2625],
+  },
+
+  {
+    text: "ਜਾਨੌ",
+    meaning: "jananaa - to know",
+    ascii: [2588, 2622, 2600, 2636],
+  },
+  {
+    text: "ਰਾਖਾ",
+    meaning: "Protector (rakhshaa - protection), caretaker",
+    ascii: [2608, 2622, 2582, 2622],
+  },
+  {
+    text: "ਸਰਨ",
+    meaning: "shelter, protection (var. sharan, sarnaaee)",
+    ascii: [2616, 2608, 2600],
+  },
+  { text: "ਰਹੋ", meaning: "stay, remain", ascii: [2608, 2617, 2635] },
+  {
+    text: "ਚਤੁਰ",
+    meaning: "clever, intelligent",
+    ascii: [2586, 2596, 2625, 2608],
+  },
+  {
+    text: "ਘਨੇਰੇ",
+    meaning: "lots, great",
+    ascii: [2584, 2600, 2631, 2608, 2631],
+  },
+  {
+    text: "ਕਹੁ",
+    meaning: "says, say \xa0(kehna - to say)",
+    ascii: [2581, 2617, 2625],
+  },
+];
