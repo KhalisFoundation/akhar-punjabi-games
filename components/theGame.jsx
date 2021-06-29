6544a07d2c6b91c22cc1a892aa96c6ff5a7c7ba2
Gian <giansingh4710@gmail.com>
2021-06-29 16:56:01 -0400

    redux try

--- components/theGame.jsx
+++ components/theGame.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react-native/no-color-literals */
-import * as React from "react";
-import * as Anvaad from "anvaad-js";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import * as React from 'react';
+import * as Anvaad from 'anvaad-js';
+import {
+  View, Text, TouchableOpacity, StyleSheet, Image
+} from 'react-native';
 // import { useSelector, useDispatch } from 'react-redux';
-import TheCircle from "./circleForGame";
-import { initialState, reducer, actions } from "../state";
+import TheCircle from './circleForGame';
+import { initialState, reducer, actions } from '../state';
 
 function GameScreen({ navigation }) {
   // const {
@@ -22,8 +24,8 @@ function GameScreen({ navigation }) {
 
   // console.log(state.correctWords);
   if (
-    state.attempt === Anvaad.unicode(state.firstWord.text, true) &&
-    state.topWord === ""
+    state.attempt === Anvaad.unicode(state.firstWord.text, true)
+    && state.topWord === ''
   ) {
     dispatch(actions.setTopWord(state.attempt));
     if (!state.correctWords.includes(state.firstWord)) {
@@ -32,8 +34,8 @@ function GameScreen({ navigation }) {
     // console.log(state.correctWords);
   }
   if (
-    state.attempt === Anvaad.unicode(state.secondWord.text, true) &&
-    state.bottomWord === ""
+    state.attempt === Anvaad.unicode(state.secondWord.text, true)
+    && state.bottomWord === ''
   ) {
     dispatch(actions.setBottomWord(state.attempt));
     if (!state.correctWords.includes(state.secondWord)) {
@@ -48,11 +50,11 @@ function GameScreen({ navigation }) {
         style={styles.backButton}
         title="Home"
         onPress={() => {
-          navigation.navigate("Home", { correctWords: state.correctWords });
+          navigation.navigate('Home', { correctWords: state.correctWords });
         }}
       >
         <Image
-          source={require("../images/left_arrow.png")}
+          source={require('../images/left_arrow.png')}
           style={styles.backArrow}
         />
       </TouchableOpacity>
@@ -111,7 +113,7 @@ function GameScreen({ navigation }) {
           style={styles.clearBox}
           onPress={() => {
             // setAttempt("");
-            dispatch(actions.setAttempt(""));
+            dispatch(actions.setAttempt(''));
           }}
         >
           <Text style={styles.clearBoxText}>CLEAR</Text>
@@ -133,28 +135,28 @@ function GameScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#5F909C",
-    paddingTop: "9%",
+    alignItems: 'center',
+    backgroundColor: '#5F909C',
+    paddingTop: '9%',
   },
   backButton: {
-    width: "10%",
-    height: "7%",
-    right: "40%",
+    width: '10%',
+    height: '7%',
+    right: '40%',
   },
   backArrow: {
-    width: "90%",
-    height: "90%",
+    width: '90%',
+    height: '90%',
   },
   title: {
     fontSize: 60,
-    bottom: "10%",
+    bottom: '10%',
   },
   wordBoxAnswers: {
     bottom: 65,
     width: 350,
     height: 250,
-    backgroundColor: "#9C734F",
+    backgroundColor: '#9C734F',
     borderRadius: 20,
   },
   hint: {
@@ -166,7 +168,7 @@ const styles = StyleSheet.create({
     height: 50,
     left: 50,
     top: 30,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 20,
   },
   answers: {
@@ -174,13 +176,13 @@ const styles = StyleSheet.create({
     left: 10,
   },
   wordAttemptView: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   wordAttempt: {
     bottom: 58,
     width: 200,
     height: 50,
-    backgroundColor: "#CFF6FF",
+    backgroundColor: '#CFF6FF',
     borderRadius: 20,
     paddingLeft: 20,
     fontSize: 30,
@@ -188,13 +190,13 @@ const styles = StyleSheet.create({
   clearBox: {
     width: 50,
     height: 30,
-    backgroundColor: "red",
+    backgroundColor: 'red',
     top: -50,
     left: 5,
     borderRadius: 20,
   },
   giveUp: {
-    backgroundColor: "red",
+    backgroundColor: 'red',
     width: 50,
     height: 50,
     borderRadius: 25,
@@ -202,14 +204,14 @@ const styles = StyleSheet.create({
     top: -20,
   },
   giveUpTxt: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   newWord: {
     left: 265,
     borderRadius: 20,
     width: 90,
-    backgroundColor: "yellow",
-    alignItems: "center",
+    backgroundColor: 'yellow',
+    alignItems: 'center',
   },
 });
 
