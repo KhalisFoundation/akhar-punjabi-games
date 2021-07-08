b43412e037b24a91dffc1d6bfaadf03c66ce806a
Gian <giansingh4710@gmail.com>
2021-07-08 18:06:36 -0400

    trying to fix testing error

--- components/landingPage.test.js
+++ components/landingPage.test.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
+import React from "react";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
 
-import configureStore from 'redux-mock-store';
-import HomeScreen from './landingPage';
+import configureStore from "redux-mock-store";
+import HomeScreen from "./landingPage";
 
 const mockStore = configureStore([]);
 
-describe('HomeScreen', () => {
+describe("HomeScreen", () => {
   let store;
 
   beforeEach(() => {
     store = mockStore({
-      myState: 'sample text',
+      myState: "sample text",
     });
   });
 
-  it('has 8 children', () => {
-    const tree = renderer.create(<Provider store={store}><HomeScreen /></Provider>).toJSON();
-    expect(tree.children.length).toBe(8);
+  it("has 9 children", () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <HomeScreen />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree.children.length).toBe(9);
   });
 });
