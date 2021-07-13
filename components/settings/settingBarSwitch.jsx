ab7be9399d5a1746150bb24673344ebf87e2e371
Gian <giansingh4710@gmail.com>
2021-07-13 13:44:59 -0400

    with switch settings

--- components/settings/settingBarSwitch.jsx
+++ components/settings/settingBarSwitch.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react-native/no-color-literals */
-import * as React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { Switch } from "react-native-elements";
-import { useSelector, useDispatch } from "react-redux";
+import * as React from 'react';
+import {
+  View, Text, StyleSheet, Image
+} from 'react-native';
+import { Switch } from 'react-native-elements';
+import { useSelector, useDispatch } from 'react-redux';
 
-import theColors from "../../util/colors";
+import theColors from '../../util/colors';
 
 function SwitchBar({
   theSetting,
@@ -22,22 +24,22 @@ function SwitchBar({
       height: 50,
     },
     settingBar: {
-      flexDirection: "row",
-      width: "100%",
-      height: "99%",
+      flexDirection: 'row',
+      width: '100%',
+      height: '99%',
       backgroundColor: colors.settingBar.settingBar,
     },
     image: {
       // flex: 1,
-      width: "30%",
-      height: "100%",
+      width: '30%',
+      height: '100%',
     },
     text1: {
       flex: 1,
     },
     rightSide: {
       flex: 1,
-      flexDirection: "row",
+      flexDirection: 'row',
     },
     text2: {
       // flex: 1,
@@ -47,9 +49,9 @@ function SwitchBar({
     },
   });
   const allImages = {
-    khalislogo150: require("../../images/khalislogo150.png"),
-    khanda: require("../../images/khanda.png"),
-    ikOngkar: require("../../images/ikOngkar.png"),
+    khalislogo150: require('../../images/khalislogo150.png'),
+    khanda: require('../../images/khanda.png'),
+    ikOngkar: require('../../images/ikOngkar.png'),
   };
   const [isVisible, setIsVisible] = React.useState(false);
   const [currentSetting, setCurrentSetting] = React.useState(
@@ -71,9 +73,9 @@ function SwitchBar({
                 // index 0 is Off and index 1 is On
                 let theTitle;
                 if (!prev === true) {
-                  theTitle = "On";
+                  theTitle = 'On';
                 } else {
-                  theTitle = "Off";
+                  theTitle = 'Off';
                 }
                 dispatch(theAction(theTitle));
                 setCurrentSetting(theTitle);
