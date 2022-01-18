fed932e6e4f1409071584f85cb69555e81e04e9a
Gurmehar Singh Khalsa <gurmeharsinghkhalsa1005@gmail.com>
2022-01-19 02:43:44 +0530

    Final update

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -15,15 +15,28 @@ import { useSelector } from 'react-redux';
 import Icon from "react-native-vector-icons/MaterialIcons";
 import GLOBAL from "../../util/globals";
 import theColors from '../../util/colors';
+import { useFonts } from 'expo-font';
+import MaskedView from '@react-native-community/masked-view';
+import { LinearGradient } from "expo-linear-gradient";
+import { Animated } from "react-native";
 
 function About({navigation}) {
   const state = useSelector((theState) => theState.theGameReducer);
+  let [fontsLoaded] = useFonts({
+    'Arial': require('../../assets/fonts/Arial.ttf'),
+    'GurbaniHeavy': require('../../assets/fonts/GurbaniAkharHeavySG.ttf'),
+    'Bookish': require('../../assets/fonts/Bookish.ttf'),
+    'Mochy': require('../../assets/fonts/Mochy.ttf'),
+    'Prabhki': require('../../assets/fonts/Prabhki.ttf'),
+  });
   const colors = theColors[state.darkMode];
   const styles = StyleSheet.create({
     container: {
       flex: 1,
+      flexDirection:'column',
       padding: 8,
       backgroundColor: "#fff",
+      height:'100%'
     },
     nightMode: {
       backgroundColor: "#000",
@@ -35,12 +48,12 @@ function About({navigation}) {
     },
     title: {
       fontSize: 40,
-      fontWeight: "bold"
     },
     small: {
       fontSize: 11
     }
   });
+  const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
   return (
     <View
@@ -74,17 +87,88 @@ function About({navigation}) {
           styles.container,
           state.darkMode && { backgroundColor: "#000" }
         ]}
+        contentContainerStyle={{
+          flexDirection:'column',
+          justifyContent:'space-between',}}
       >
-        <Text
-          style={[styles.title, state.darkMode && styles.nightMode]}
-        >
-          ਅਖਰ ਜੋੜ
+        <View>
+        <MaskedView
+            style={{height: 50, width:"100%"}}
+            maskElement={
+              <View
+                style={{
+                  backgroundColor: 'transparent',
+                }}>
+                <Text style={[styles.title, state.darkMode && styles.nightMode, {fontFamily:'Bookish'}]} > ਅਖਰ ਜੋੜ</Text>
+              </View>
+            }>
+            <LinearGradient
+              colors={state.darkMode?["#ff8008", "#ffc837"]: ["#FF0076", "#590FB7"]}
+              style={{ flex: 1 }}
+            />
+          </MaskedView>
+          <MaskedView
+            style={{height: 50, width:"100%", marginTop:0}}
+            maskElement={
+              <View
+                style={{
+                  backgroundColor: 'transparent',
+                }}>
+                <Text style={{fontFamily:'Mochy', margin:10}}>(Akhar Jor)</Text>
+              </View>
+            }>
+            <LinearGradient
+              colors={state.darkMode? ["#FF0076", "#590FB7"]: ["#ff8008", "#ffc837"]}
+              style={{ flex: 1 }}
+            />
+          </MaskedView>
+          </View>
+          <Image style={{height:250, alignSelf:'center'}} source={require('../../images/logo_squared.png')} resizeMode="contain"/>
+        <Text style={[state.darkMode && styles.nightMode, {fontSize:16, fontFamily:'Arial'}]}>
+          {"\n"}
+          <Text style={{fontFamily:'Mochy'}}>Akhar Jor - Punjabi Wordlink</Text> utilizes{" \n\n \u2022"}
+          <Text
+            style={{ color: "#009bff", }}
+            onPress={() => Linking.openURL("https://www.discoversikhism.com/sikh.html")}
+          >
+            DiscoverSikhism
+          </Text>{", \n \u2022"}
+          <Text
+            style={{ color: "#009bff" }}
+            onPress={() => Linking.openURL("https://1000mostcommonwords.com/1000-most-common-punjabi-words/")}
+          >
+            1000MostCommonWords
+          </Text>{", \n \u2022"}
+          <Text
+            style={{ color: "#009bff" }}
+            onPress={() => Linking.openURL("https://www.chardikalaa.com/?page_id=61")}
+          >
+            Chardikalaa
+          </Text>{", \n \u2022"}
+          <Text
+            style={{ color: "#009bff" }}
+            onPress={() => Linking.openURL("https://www.sikhiwiki.org/index.php/Gurmukhi_to_English")}
+          >
+            SikhiWiki
+          </Text>{" \n"}
+          to create Punjabi Wordlink, an interactive game, for spreading the knowledge of our Mother Tongue, Punjabi. {"\n"}
         </Text>
-        <Text
-          style={[styles.small, state.darkMode && styles.nightMode]}
-        >
-          {"\n"}Created By:
+        <Text style={[state.darkMode && styles.nightMode, {fontSize:16, fontFamily:'Arial'}]}>
+            {"\n"}We welcome your comments, suggestions, and corrections!
+            For information, suggestions, or help, visit us at{"  "}
+          <Text
+              style={{ color: "#009bff", fontFamily:'Mochy' }}
+              onPress={() => Linking.openURL("https://khalisfoundation.org")}
+            >
+              KhalisFoundation.org
+            </Text>
+            {"\n"}
         </Text>
+        <View>
+        <Text style={state.darkMode && styles.nightMode, {fontFamily:'Arial', alignSelf:'flex-end'}}>
+          {"\n"}Bhul Chuk Maaf!{"\n"}
+        </Text>
+        
         <TouchableHighlight
           underlayColor={"#009bff"}
           onPress={() => Linking.openURL("https://khalisfoundation.org")}
@@ -95,75 +179,25 @@ function About({navigation}) {
                 ? require("../../images/khalislogo150white.png")
                 : require("../../images/khalislogo150.png")
             }
+            style={{ width: 125 }}
+            resizeMode="contain"
           />
         </TouchableHighlight>
-        <Text style={state.darkMode && styles.nightMode}>
-          <Text>
-            {"\n"}We welcome your comments, suggestions, and corrections!
-            {"\n"}
-            {"\n"}
-            For information, suggestions, or help, visit us at{"\n"}
-          </Text>
-          <Text>
-            <Text
-              style={{ color: "#009bff" }}
-              onPress={() => Linking.openURL("https://khalisfoundation.org")}
-            >
-              https://www.KhalisFoundation.org
-            </Text>
-            <Text>!</Text>
-          </Text>
-        </Text>
-        <Text style={state.darkMode && styles.nightMode}>
-          {"\n"}
-          {"\n"}
-          Akhar Jod utilizes{" "}
-          <Text
-            style={{ color: "#009bff" }}
-            onPress={() => Linking.openURL("https://www.discoversikhism.com/sikh.html")}
-          >
-            DiscoverSikhism
-          </Text>{", "}
-          <Text
-            style={{ color: "#009bff" }}
-            onPress={() => Linking.openURL("https://1000mostcommonwords.com/1000-most-common-punjabi-words/")}
-          >
-            1000MostCommonWords
-          </Text>{", "}
-          <Text
-            style={{ color: "#009bff" }}
-            onPress={() => Linking.openURL("https://www.chardikalaa.com/?page_id=61")}
-          >
-            Chardikalaa
-          </Text>{", "}
-          <Text
-            style={{ color: "#009bff" }}
-            onPress={() => Linking.openURL("https://www.sikhiwiki.org/index.php/Gurmukhi_to_English")}
-          >
-            SikhiWiki
-          </Text>{" "}
-          - to create Gurmukhi Wordlink, an interactive game, for spreading the knowledge of our Mother Tongue, Punjabi. {"\n"}
-        </Text>
-        <Text style={state.darkMode && styles.nightMode}>
-          {"\n"}Bhul Chuk Maaf!{"\n"}
-        </Text>
-        <Text
-          style={[styles.small, state.darkMode && styles.nightMode]}
-        />
 
         <View style={styles.singleLine}>
           <View style={styles.leftContainer}>
             <Text
-              style={[styles.small, state.darkMode && styles.nightMode]}
+              style={[styles.small, state.darkMode && styles.nightMode, {fontFamily:'Arial'}]}
             >
               &copy; {new Date().getFullYear()} Khalis Foundation
             </Text>
           </View>
           <Text
-            style={[styles.small, state.darkMode && styles.nightMode]}
+            style={[styles.small, state.darkMode && styles.nightMode, {fontFamily:'Arial'}]}
           >
             Chardikala{"\n"}
           </Text>
+        </View>
         </View>
       </ScrollView>
     </View>
