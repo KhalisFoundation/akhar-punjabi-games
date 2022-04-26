7df3df4f915ed61d45b902bcee5ee32287a88d79
Gurmehar Singh Khalsa <gurmeharsinghkhalsa1005@gmail.com>
2022-04-27 04:56:18 +1000

    Add files via upload

---
+++ components/landingPage.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HomeScreen from './homeScreen/landingPage';
+
+describe('<HomeScreen />', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<HomeScreen />).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
+  it('renders correctly', () => {
+    const tree = renderer.create(<HomeScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
