3e41dd824f1fdfeb5fdb287e4334eecd1609415e
Gurmehar Singh <gsingh@waheguroo.com>
2025-09-26 02:55:33 +1000

    fix: add firebase file to run test in CI

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+
+export { auth, firebase };
